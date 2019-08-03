bdf31c12e8927be79d69bd17e51a44630a92a845
raghurajj <raghurajpratap1999@gmail.com>
2019-08-03 16:18:42 +0530

    deployed the website

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import {MovieProvider} from "./Context";
 import * as serviceWorker from './serviceWorker';
 import 'font-awesome/css/font-awesome.min.css';

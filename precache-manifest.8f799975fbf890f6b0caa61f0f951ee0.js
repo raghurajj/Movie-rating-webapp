6724129276e7dd22a1ce67970a22ef283dd38548
raghurajj <raghurajpratap1999@gmail.com>
2019-08-03 16:09:00 +0530

    Updates

---
+++ precache-manifest.8f799975fbf890f6b0caa61f0f951ee0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db88224ef61f1666080391e5942d2f9",
+    "url": "/Movie-rating-webapp/index.html"
+  },
+  {
+    "revision": "5308362dc6b3d1118ddd",
+    "url": "/Movie-rating-webapp/static/css/2.28d6eb5c.chunk.css"
+  },
+  {
+    "revision": "7895c303ba4af8287584",
+    "url": "/Movie-rating-webapp/static/css/main.0f92fea6.chunk.css"
+  },
+  {
+    "revision": "5308362dc6b3d1118ddd",
+    "url": "/Movie-rating-webapp/static/js/2.60970536.chunk.js"
+  },
+  {
+    "revision": "7895c303ba4af8287584",
+    "url": "/Movie-rating-webapp/static/js/main.cbfb1738.chunk.js"
+  },
+  {
+    "revision": "e9ed434dc5ee7437ae74",
+    "url": "/Movie-rating-webapp/static/js/runtime~main.a224d751.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Movie-rating-webapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Movie-rating-webapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Movie-rating-webapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Movie-rating-webapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Movie-rating-webapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
